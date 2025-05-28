d767e2f79424f4ba09ca516ff60643eba6dcc0c4
ailisu <51803220+ailisu@users.noreply.github.com>
2025-05-28 22:48:58 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,6 @@
-const specificDate = new Date(2025, 4, 28);  
-const year = specificDate.getFullYear();
-const month = specificDate.getMonth() + 1;
-const day = specificDate.getDate();
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
 
-const formattedDate = year + '年' + month + '月' + day + '日';
-console.log(formattedDate);
+console.log(year + '年' + month + '月' + date + '日');
